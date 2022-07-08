90238b9bff918ebba569bb3be335fa3e0fd5dd03
Tito Anugerah Maharizky <tito.anugerah@epson.co.id>
2022-07-08 10:26:09 +0700

    Added Role Model

--- Thunder/Models/Role.cs
+++ Thunder/Models/Role.cs
@@ -2,16 +2,11 @@
 
 namespace Thunder.Models
 {
-    public class User
+    public class Role
     {
         [Key]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
-        [Required]
-        public string Email { get; set; }
-        [Required]
-        public string RoleId { get; set; }
-        public Role Role { get; set; }
     }
 }
