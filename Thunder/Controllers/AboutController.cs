0a6e46a381a9da71e9d7e5ba6c58acf289103448
titoanugerah <titoanugerah@student.undip.ac.id>
2022-07-14 02:08:50 +0700

    Added about us page

---
+++ Thunder/Controllers/AboutController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Thunder.DataAccess;
+
+namespace Thunder.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly ILogger<AboutController> logger;
+        private readonly ThunderDB thunderDB;
+
+        public AboutController(ILogger<AboutController> _logger, ThunderDB _thunderDB)
+        {
+            logger = _logger;
+            thunderDB = _thunderDB;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                int totalUser = thunderDB.User
+                    .Count();
+                int newUser = thunderDB.User
+                    .Where(column => column.CreatedDate.Value.Date == DateTime.Now.Date)
+                    .Count();
+                ViewBag.StatisticUser = newUser/totalUser;
+                return View();
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error, "About Controller - Index");
+                throw;
+            }
+        }
+    }
+}
