ed80e0b59f29d254268129086f070340463d8867
titoanugerah <titoanugerah@student.undip.ac.id>
2022-07-14 01:43:34 +0700

    Added Profile Page

---
+++ Thunder/Controllers/ProfileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Thunder.DataAccess;
+
+namespace Thunder.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ILogger<ProfileController> logger;
+        private readonly ThunderDB thunderDB;
+
+        public ProfileController(ILogger<ProfileController> _logger, ThunderDB _thunderDB)
+        {
+            logger = _logger;
+            thunderDB = _thunderDB;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error, "Profile Controller - Index");
+                throw;
+            }
+        }
+    }
+}
