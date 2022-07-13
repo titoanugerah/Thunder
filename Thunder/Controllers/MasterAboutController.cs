41da3dfc9347d09033310add2ff377368aeb4360
titoanugerah <titoanugerah@student.undip.ac.id>
2022-07-14 02:23:36 +0700

    Added controller master about

---
+++ Thunder/Controllers/MasterAboutController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Thunder.DataAccess;
+
+namespace Thunder.Controllers
+{
+    public class MasterAboutController : Controller
+    {
+        private readonly ILogger<MasterAboutController> logger;
+        private readonly ThunderDB thunderDB;
+
+        public MasterAboutController(ILogger<MasterAboutController> _logger, ThunderDB _thunderDB)
+        {
+            logger = _logger;
+            thunderDB = _thunderDB;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error, "Master About Controller - Index");
+                throw;
+            }
+        }
+    }
+}
