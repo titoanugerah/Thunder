e81e443b1661d97ab225ccc65608b422104d883e
titoanugerah <titoanugerah@student.undip.ac.id>
2022-07-15 00:15:19 +0700

    Added page master user

---
+++ Thunder/Controllers/MasterUserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Thunder.DataAccess;
+
+namespace Thunder.Controllers
+{
+    public class MasterUserController : Controller
+    {
+        private readonly ILogger<MasterUserController> logger;
+        private readonly ThunderDB thunderDB;
+
+        public MasterUserController(ILogger<MasterUserController> _logger, ThunderDB _thunderDB)
+        {
+            logger = _logger;
+            thunderDB = _thunderDB;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                ViewBag.Users = thunderDB.User
+                    .Include(table => table.Role)
+                    .OrderBy(column => column.Name)
+                    .ToList();
+                return View();
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error, "Master About Controller - Index");
+                throw;
+            }
+        }
+
+    }
+}
