cbaf3afbe17e2465ccffb4398c737ab174f2ef42
titoanugerah <titoanugerah@student.undip.ac.id>
2022-08-19 22:32:48 +0700

    Added controller Finder

---
+++ Thunder/Controllers/FinderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Thunder.Controllers
+{
+    public class FinderController : Controller
+    {
+
+        private readonly ILogger<FinderController> logger;
+
+        public FinderController(ILogger<FinderController> _logger)
+        {
+            logger = _logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error, "Finder Controller - Index");
+                throw;
+            }
+        }
+    }
+}
