180604507cd63e7a7581b7ac96536d33ab22e583
titoanugerah <titoanugerah@student.undip.ac.id>
2022-08-20 01:42:29 +0700

    Added controller and view of City

---
+++ Thunder/Controllers/MasterCityController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Thunder.Controllers
+{
+    public class MasterCityController : Controller
+    {
+        private readonly ILogger<MasterCityController> logger;
+        public MasterCityController(ILogger<MasterCityController> _logger)
+        {
+            logger = _logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error, "Master City Controller - Index");
+                throw;
+            }
+        }
+
+    }
+}
