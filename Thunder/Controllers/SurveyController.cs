48f632b12184bc8d06c5d58c905b0a8e4c199ff9
titoanugerah <titoanugerah@student.undip.ac.id>
2022-09-09 01:01:29 +0700

    Added new controller : Survey controller

---
+++ Thunder/Controllers/SurveyController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Thunder.DataAccess;
+
+namespace Thunder.Controllers
+{
+    public class SurveyController : Controller
+    {
+        private readonly ILogger<SurveyController> logger;
+        private readonly ThunderDB thunderDB;
+
+        public SurveyController(ILogger<SurveyController> logger, ThunderDB thunderDB)
+        {
+            this.logger = logger;
+            this.thunderDB = thunderDB;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                return View();
+            }
+            catch (Exception error)
+            {
+                logger.LogError(error, $"Survey Controller - Index");
+                throw;
+            }
+        }
+
+        
+    }
+}
