633fa4f4541339c11d1e0396f5bb9c8359bcf96a
titoanugerah <titoanugerah@student.undip.ac.id>
2022-09-09 01:29:26 +0700

    Added model survey

---
+++ Thunder/Models/Survey.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace Thunder.Models
+{
+    public class Survey : Audit
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { set; get; }
+        [DefaultValue(1)]
+        public int PricePriority { set; get; }
+        [DefaultValue(1)]
+        public int FacilityPriority { set; get; }
+        [DefaultValue(1)]
+        public int CityPriority { set; get; }
+        [DefaultValue(1)]
+        public int AccreditationPriority { set; get; }
+
+    }
+}
